6e94bc3514cec367f4898dde7ffe780ac68f13a4
rickeywon <rickeywon@163.com>
2019-12-08 22:32:05 +0800

    fix: add essential components

--- binary/cli/src/main.rs
+++ binary/cli/src/main.rs
@@ -1,53 +1,23 @@
-extern crate clap;
-use clap::{Arg, App, SubCommand};
+#![warn(missing_docs)]
+#![warn(unused_extern_crates)]
 
-fn main() {
-    let matches = App::new("GMPC network daemon")
-                          .version(env!("VERGEN_SEMVER"))
-                          .author("GMPC contributors <contact@gm-pc.com>")
-                          .about("GMPC distributed network")
-                          .arg(Arg::with_name("config")
-                               .short("c")
-                               .long("config")
-                               .value_name("FILE")
-                               .help("Sets a custom config file")
-                               .takes_value(true))
-                          .arg(Arg::with_name("INPUT")
-                               .help("Sets the input file to use")
-                               .required(true)
-                               .index(1))
-                          .arg(Arg::with_name("v")
-                               .short("v")
-                               .multiple(true)
-                               .help("Sets the level of verbosity"))
-                          .get_matches();
+mod genesis;
+mod service;
+mod cli;
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+pub use substrate_cli::{VersionInfo, IntoExit, error};
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
-    }
-
-    // more program logic goes here...
+fn run() -> cli::error::Result<()> {
+	let version = VersionInfo {
+		name: "Substrate Node",
+		commit: env!("VERGEN_SHA_SHORT"),
+		version: env!("CARGO_PKG_VERSION"),
+		executable_name: "gmpc",
+		author: "gmpc-authors",
+		description: "gmpc",
+		support_url: "support.anonymous.an",
+	};
+	cli::run(::std::env::args(), cli::Exit, version)
 }
+
+error_chain::quick_main!(run);
---
+++ component/wasm/src/lib.rs
@@ -0,0 +1,5 @@
+//! reexported for WebAssembly compile.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use gmpc_runtime::*;
